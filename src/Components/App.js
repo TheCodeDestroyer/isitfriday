49ff3734be2cef0b4969d9b44a3c770d7cf39810
Nace Logar <the.code.destroyer@gmail.com>
2017-06-21 19:29:12 +0200

    Cleanup app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,14 @@
-import { Component } from 'react';
+import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import { appTemplate } from './App.template';
 
-class App extends Component {
-    shouldComponentUpdate() {
-        return true;
-    }
-
-    render() {
-        return appTemplate(logo);
-    }
-}
+const App = () => (
+    <div className="App">
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>{'Welcome to React'}</h2>
+        </div>
+    </div>
+);
 
 export default App;
