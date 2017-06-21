a5bb6bc16d88a55915c4901d326f628b7ff111a7
Nace Logar <the.code.destroyer@gmail.com>
2017-06-21 21:16:53 +0200

    Finalized Gif component

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class Gif extends React.Component {
     constructor(props) {
         super(props);
         this.dayOfWeek = props.weekday;
+        this.gifUrl = 'http://media4.giphy.com/media/N256GFy1u6M6Y/giphy.gif';
+        this.giphyApiKey = 'not gonna put that here ofc';
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${this.giphyApiKey}&rating=G&tag=${this.dayOfWeek}`)
+            .then((response) => {
+                this.gifUrl = response.data.data.image_url;
+                setTimeout(() => {
+                    this.forceUpdate();
+                }, 500);
+            });
     }
 
     shouldComponentUpdate() {
@@ -14,7 +27,8 @@ class Gif extends React.Component {
     render() {
         return (
             <div className="iff-gif">
-                {`GIF ${this.dayOfWeek}`}
+                <img src={this.gifUrl} alt="Gif" title="Gif" />
+                <br/>
             </div>
         );
     }
