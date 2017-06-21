9ff4ad3cb1d833838e4fc5a59ef73a0da74153a2
Nace Logar <the.code.destroyer@gmail.com>
2017-06-21 20:26:33 +0200

    Switch function and component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
 import IsItFriday from './IsItFriday';
+import moment from 'moment';
 
-const App = () => (
-    <div className="App">
-        <div className="App-header">
-            <h1>{'Is it friday?'}</h1>
-        </div>
-        <IsItFriday/>
-    </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.currentDate = moment();
+        this.isItFriday = this.currentDate.isoWeekday() === 5;
+        this.dayOfWeek = this.currentDate.format('dddd');
+    }
+
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>{'Is it friday?'}</h1>
+                </div>
+                <IsItFriday friday={this.isItFriday} weekday={this.dayOfWeek}/>
+            </div>
+        );
+    }
+}
 
 export default App;
--- src/Components/IsItFriday.js
+++ src/Components/IsItFriday.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import moment from 'moment';
 
-class IsItFriday extends React.Component {
-    constructor(props) {
-        super(props);
-        this.currentDate = moment();
-        this.isItFriday = this.currentDate.isoWeekday() === 5;
-        this.dayOfWeek = this.currentDate.format('dddd');
-    }
-
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    render() {
-        return (
-            <div className="iif-answer">
-                <h1>{this.isItFriday ? 'Yes!' : 'No'}</h1>
-                <h2>{`It's ${this.dayOfWeek}...`}</h2>
-            </div>
-        );
-    }
-}
+const IsItFriday = (props) => (
+    <div className="iif-answer">
+        <h1>{props.friday ? 'Yes!' : 'No'}</h1>
+        <h2>{`It's ${props.weekday}...`}</h2>
+    </div>
+);
 
 export default IsItFriday;
