6518ec3e90f4c72eb854642622a579b432c387fc
Nace Logar <the.code.destroyer@gmail.com>
2017-06-23 09:14:40 +0200

    Add conditional rendering

--- src/Components/IsItFriday.js
+++ src/Components/IsItFriday.js
@@ -3,7 +3,9 @@ import React from 'react';
 const IsItFriday = (props) => (
     <div className="iif-answer">
         <h1>{props.friday ? 'Yes!' : 'No'}</h1>
+        {!props.friday &&
         <h2>{`It's ${props.weekday}...`}</h2>
+        }
     </div>
 );
 
