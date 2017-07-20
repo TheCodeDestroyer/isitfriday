e649f2294d5227533763472c0a51972fd3cc4830
Nace Logar <the.code.destroyer@gmail.com>
2017-07-20 09:01:24 +0200

    Change linter and fix some linting issues

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,30 +6,30 @@ import Footer from './Footer';
 import moment from 'moment';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.currentDate = moment();
-        this.isItFriday = this.currentDate.isoWeekday() === 5;
-        this.dayOfWeek = this.currentDate.format('dddd');
-        this.year = this.currentDate.format('YYYY');
-    }
+  constructor(props) {
+    super(props);
+    this.currentDate = moment();
+    this.isItFriday = this.currentDate.isoWeekday() === 5;
+    this.dayOfWeek = this.currentDate.format('dddd');
+    this.year = this.currentDate.format('YYYY');
+  }
 
-    shouldComponentUpdate() {
-        return false;
-    }
+  shouldComponentUpdate() {
+    return false;
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <h1>{'Is it friday?'}</h1>
-                </div>
-                <IsItFriday friday={this.isItFriday} weekday={this.dayOfWeek}/>
-                <Gif weekday={this.dayOfWeek}/>
-                <Footer year={this.year}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>{'Is it friday?'}</h1>
+        </div>
+        <IsItFriday friday={this.isItFriday} weekday={this.dayOfWeek}/>
+        <Gif weekday={this.dayOfWeek}/>
+        <Footer year={this.year}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/IsItFriday.js
+++ src/Components/IsItFriday.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const IsItFriday = (props) => (
-    <div className="iif-answer">
-        <h1>{props.friday ? 'Yes!' : 'No'}</h1>
-        {!props.friday &&
-        <h2>{`It's ${props.weekday}...`}</h2>
-        }
-    </div>
+  <div className="iif-answer">
+    <h1>{props.friday ? 'Yes!' : 'No'}</h1>
+    {!props.friday &&
+    <h2>{`It's ${props.weekday}...`}</h2>
+    }
+  </div>
 );
+
+IsItFriday.propTypes = {
+  friday: PropTypes.bool,
+  weekday: PropTypes.string.isRequired
+};
+
+IsItFriday.defaultProps = { friday: false };
 
 export default IsItFriday;
