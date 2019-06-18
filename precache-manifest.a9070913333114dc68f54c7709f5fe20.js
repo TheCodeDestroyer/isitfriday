112582674cbca5dc2cd3db3f4ab438ec317582ab
Nace Logar <the.code.destroyer@gmail.com>
2019-06-18 16:31:23 +0200

    Update

---
+++ precache-manifest.a9070913333114dc68f54c7709f5fe20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa49df45db92635347ebda123b660fba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302ae01b3993ab48f067",
+    "url": "/static/css/main.ad7ca424.chunk.css"
+  },
+  {
+    "revision": "8d8015a602270b71e2fe",
+    "url": "/static/js/2.1fd328f9.chunk.js"
+  },
+  {
+    "revision": "302ae01b3993ab48f067",
+    "url": "/static/js/main.006655dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
