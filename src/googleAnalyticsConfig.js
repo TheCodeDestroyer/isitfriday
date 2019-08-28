c406421977189437e5667b5856d6fd81ca1f28c8
Nace Logar <the.code.destroyer@gmail.com>
2019-08-28 19:42:20 +0200

    Implement google analytics

---
+++ src/googleAnalyticsConfig.js
@@ -0,0 +1,4 @@
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-146578532-1');
+ReactGA.pageview(window.location.pathname);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './Components/App';
+
+import './googleAnalyticsConfig';
 import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
