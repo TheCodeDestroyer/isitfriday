ac894885babfaf8be53cda6973f57ae8203cc24c
Nace Logar <the.code.destroyer@gmail.com>
2022-05-26 14:52:47 +0200

    Add option to force

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,30 @@ import moment from 'moment';
 import IsItFriday from './component/IsItFriday';
 import Gif from './component/Gif';
 import Footer from './component/Footer';
+import { includes, lowerCase, words } from 'lodash';
 
 const usePrepareData = () => {
+  const { pathname } = window.location;
+
+  const isForced = useMemo(() => {
+    const keywords = words(pathname);
+    const lowerKeywords = lowerCase(keywords);
+
+    return includes(lowerKeywords, 'f');
+  }, [pathname]);
+
   const currentDate = useMemo(() => moment(), []);
-  const dayOfWeek = useMemo(() => currentDate.format('dddd'), [currentDate]);
+  const dayOfWeek = useMemo(() => {
+    if (isForced) {
+      return 'Friday';
+    }
+
+    return currentDate.format('dddd');
+  }, [currentDate, isForced]);
   const year = useMemo(() => currentDate.format('YYYY'), [currentDate]);
   const isItFriday = useMemo(
-    () => currentDate.isoWeekday() === 5,
-    [currentDate]
+    () => currentDate.isoWeekday() === 5 || isForced,
+    [currentDate, isForced]
   );
 
   return { dayOfWeek, year, isItFriday };
@@ -23,7 +39,7 @@ const App = () => {
   return (
     <div className="app">
       <div className="app-header">
-        <h1>Is it friday?</h1>
+        <h1>Is it Friday?</h1>
       </div>
       <IsItFriday friday={isItFriday} weekday={dayOfWeek} />
       <Gif weekday={dayOfWeek} />
