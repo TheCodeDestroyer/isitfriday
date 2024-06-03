2dcd6babc8c4588eab8fae0aa431337a5c7dafc1
Nace Logar <the.code.destroyer@gmail.com>
2024-06-03 13:48:59 +0200

    style: Format code

--- components/HeadMeta.tsx
+++ components/HeadMeta.tsx
@@ -4,7 +4,10 @@ import type { FC } from 'react';
 export const HeadMeta: FC = () => (
   <Head>
     <title>Is It Friday? | Find Out Now & Enjoy Daily Memes!</title>
-    <meta name="description" content="Isitfriday.eu tells you whether it's Friday or not in a flash! For every other day, get a smile with a daily meme gif tailored to the day. Check now and start your day with a dose of fun!" />
+    <meta
+      name="description"
+      content="Isitfriday.eu tells you whether it's Friday or not in a flash! For every other day, get a smile with a daily meme gif tailored to the day. Check now and start your day with a dose of fun!"
+    />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
 
